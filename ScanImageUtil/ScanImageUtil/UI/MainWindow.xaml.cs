a1146495e97a1a612e88595aa0d36311b10a8b44
Dmytro Porokhovnik <dmytrox.porokhovnik@intel.com>
2020-02-07 13:20:49 +0200

    UI main features

--- ScanImageUtil/ScanImageUtil/UI/MainWindow.xaml.cs
+++ ScanImageUtil/ScanImageUtil/UI/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +23,51 @@ namespace ScanImageUtil
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Regex numberRegex = new Regex("[^0-9]+");
+
         public MainWindow()
         {
             InitializeComponent();
+            targetFormat.ItemsSource = new string[] { ".png", ".jpeg", ".jpg" };
+            targetFormat.SelectedItem = ".jpg";
+        }
+
+        private void Choose_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg",
+                Multiselect = true
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                chosedFiles.ItemsSource = openFileDialog.FileNames;              
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CompressNeedChanged(object sender, RoutedEventArgs e)
+        {
+            if ((sender as CheckBox).IsChecked == true)
+                qualityPanel.Visibility = Visibility.Visible;
+            else
+                qualityPanel.Visibility = Visibility.Hidden;
+        }
+
+        private void QualityPercentageChanged(object sender, TextChangedEventArgs e)
+        {
+            var txtBox = (sender as TextBox);
+            var isCorrectNumber = !numberRegex.IsMatch(txtBox.Text);
+           
+            if (!isCorrectNumber || Int32.Parse(txtBox.Text) > 100 || Int32.Parse(txtBox.Text) < 0)
+            {            
+                MessageBox.Show("Quality percentage should be only a number(0-100)", "Wrong input!", MessageBoxButton.OK, MessageBoxImage.Error);
+                txtBox.Text = "50"; // default value
+            }
         }
     }
 }
