374866c2f30175348baf482c79d4ec1fe62d2374
Dmytro Porokhovnik <dmytrox.porokhovnik@intel.com>
2020-02-24 18:07:31 +0200

    date validation

--- ScanImageUtil/ScanImageUtil/Back/Helper.cs
+++ ScanImageUtil/ScanImageUtil/Back/Helper.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace ScanImageUtil.Back
 {
@@ -15,8 +12,36 @@ namespace ScanImageUtil.Back
             if (fileNameParts.Length != 5)
                 return false;
 
-            if (fileNameParts[1].Length != 6)
+            if (fileNameParts[1].Length != 6 && fileNameParts[1].Length != 8)
                 return false;
+            else
+            {
+              
+                if (fileNameParts[1].Length == 6)
+                {
+                    try
+                    {
+                        var dateString = fileNameParts[1].Substring(0, 2) + '/' + fileNameParts[1].Substring(2, 2) + '/' + fileNameParts[1].Substring(4, 2);
+                        DateTime.ParseExact(dateString, "dd/MM/yy", CultureInfo.InvariantCulture);
+                    }
+                    catch (Exception)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        var dateString = fileNameParts[1].Substring(0, 2) + '/' + fileNameParts[1].Substring(2, 2) + '/' + fileNameParts[1].Substring(4, 4);
+                        DateTime.ParseExact(dateString, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                    }
+                    catch (Exception)
+                    {
+                        return false;
+                    }
+                }
+            }
 
             if (string.IsNullOrEmpty(fileNameParts[3]) || string.IsNullOrEmpty(fileNameParts[4]))
                 return false;
